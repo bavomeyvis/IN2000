276214e5e0169b99632169d9fb3532bd08a93f81
bavomeyvis <bavomeyvis@gmail.com>
2019-05-07 15:35:34 +0200

    Bugs removed. Features disabled of Google Maps. Mylocation button, rotate button etc.

--- app/src/main/java/com/example/pollution/ui/MapsActivity.kt
+++ app/src/main/java/com/example/pollution/ui/MapsActivity.kt
@@ -125,10 +125,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, PopupMenu.OnMenuIt
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+
         // https://stackoverflow.com/questions/6250325/hide-google-logo-from-mapview/6250405
         // Turns off the two buttons in the bottom right (directions and open maps)
         mMap.uiSettings.isMapToolbarEnabled = false
-        mMap.uiSettings.isMyLocationButtonEnabled = true
+        mMap.uiSettings.isMyLocationButtonEnabled = false
+        mMap.uiSettings.isCompassEnabled = false
+        mMap.uiSettings.isZoomControlsEnabled = false
 
         val boundOne = LatLng(57.696784, 3.601294)
         val boundTwo = LatLng(71.214304, 34.476990)
