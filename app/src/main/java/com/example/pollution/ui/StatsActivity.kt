c85f94ebf65fc5520dbfb1f684988db827217890
bavomeyvis <bavomeyvis@gmail.com>
2019-05-10 13:23:20 +0200

    Pimped code. Bravo.

--- app/src/main/java/com/example/pollution/ui/StatsActivity.kt
+++ app/src/main/java/com/example/pollution/ui/StatsActivity.kt
@@ -5,18 +5,11 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Toast
 import com.example.pollution.R
-import com.example.pollution.classes.City
 import com.example.pollution.response.WeatherService
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
-import org.jetbrains.anko.custom.async
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
-import org.w3c.dom.Text
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.math.BigDecimal
@@ -25,52 +18,26 @@ import java.math.RoundingMode
 class StatsActivity : AppCompatActivity() {
     companion object {
         var perCountryAQI : HashMap<Double, String> = hashMapOf()
-        var count : Int = 0
+        var count : Int = 1
+        var nCountries : Int = 0
     }
-    var moreData = true
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        // Sets theme
         if(getSharedPreferenceValue("theme")) setTheme(R.style.DarkTheme)
         else setTheme(R.style.AppTheme)
-
+        // Sets layout
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
-
+        // Makes a client connection
         val client = Retrofit.Builder()
             .baseUrl("https://in2000-apiproxy.ifi.uio.no/weatherapi/")
             .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(WeatherService::class.java)
-
+            .build().create(WeatherService::class.java)
+        // Loops through received list of countries and gets countryValues
         val countries : HashMap<String, LatLng> = intent.extras["hashMap"] as HashMap<String, LatLng>
-
-        for ((key, value) in countries) {
-            getValue(client, key, value)
-            // perCountryAQI[key] = getValue(client, key, value)
-        }
-
+        nCountries = countries.size
+        for ((key, value) in countries) getCountryValue(client, key, value)
     }
-    private fun getValue(client : WeatherService, key : String, value : LatLng)  {
-        doAsync {
-            val weather = client.getWeather(value.latitude, value.longitude).execute().body()
-            val aqi = weather?.data?.time?.get(0)?.variables?.aQI?.value
-
-            uiThread {
-                if (aqi != null) {
-                    this@StatsActivity.setValue(key, aqi)
-
-
-                }
-                else Toast.makeText(this@StatsActivity, "Shit", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-    private fun setValue(country : String, aqiValue : Double)  {
-        count++
-        perCountryAQI[aqiValue] = country
-        if(count == 18) setRanking(perCountryAQI)
-    }
-
 
     // TODO: Repetitive code
     private fun getSharedPreferenceValue(prefKey: String):Boolean {
@@ -78,10 +45,25 @@ class StatsActivity : AppCompatActivity() {
         return sp.getBoolean(prefKey, false)
     }
 
+    private fun getCountryValue(client : WeatherService, key : String, value : LatLng)  {
+        doAsync {
+            val weather = client.getWeather(value.latitude, value.longitude).execute().body()
+            val aqi = weather?.data?.time?.get(0)?.variables?.aQI?.value
+            uiThread {
+                if (aqi != null) it.addCountryValue(key, aqi)
+            }
+        }
+    }
+    // Adds value to the second hashMap of countries
+    private fun addCountryValue(country : String, aqiValue : Double)  {
+        perCountryAQI[aqiValue] = country
+        if(count == nCountries) setRanking(perCountryAQI)
+        else count++
+    }
     private fun setRanking(countriesAndValues : HashMap<Double, String>) {
+        // Sorts list
         val sortedMap = countriesAndValues.toSortedMap(compareByDescending { it })
         val statsTable : LinearLayout = findViewById(R.id.stats_list)
-
         var i = 0
         for ((key, value) in sortedMap) {
             i++
@@ -97,8 +79,6 @@ class StatsActivity : AppCompatActivity() {
                     item1.text =  decimal.toString()
                 }
             }
-
         }
     }
-
 }
