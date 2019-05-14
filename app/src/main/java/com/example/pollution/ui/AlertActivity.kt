b5b0abdbdf76694938b901fb7f272b157244a39e
tobiafro <tobiafro@student.matnat.uio.no>
2019-05-14 17:21:55 +0200

    Mostly done with design. Still lacking an exclude cities box. It seems to be very difficult to implement, and may be best if ignored in order to focus resources on more pressing matters. Next step will be adding functionality.

--- app/src/main/java/com/example/pollution/ui/AlertActivity.kt
+++ app/src/main/java/com/example/pollution/ui/AlertActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.transition.Slide
@@ -17,6 +18,7 @@ import com.example.pollution.R
 
 class AlertActivity : AppCompatActivity() {
     private val returnIntent = Intent()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // Displays activity_alert
         super.onCreate(savedInstanceState)
@@ -25,7 +27,7 @@ class AlertActivity : AppCompatActivity() {
         // Threshold bar
         val threshold: SeekBar = findViewById(R.id.seekBarThreshold)
         threshold.max = 500
-        threshold.progress = 100
+        threshold.progress = getSharedPreferenceValueInt("thresholdValue")
         var start = 0
         var end = threshold.progress
         var current = threshold.progress
@@ -35,8 +37,10 @@ class AlertActivity : AppCompatActivity() {
                 seekBar: SeekBar, progress: Int,
                 fromUser: Boolean
             ) {
+                writeToPreferenceInt("thresholdValue", progress)
                 // Reference from source: https://airnow.gov/index.cfm?action=aqibasics.aqi
                 if (current !in start..end) {
+                    // Cancel toast unless a new AQI level is reached.
                     toast.cancel()
                     when (progress) {
                         in 0..50 -> {
@@ -80,10 +84,13 @@ class AlertActivity : AppCompatActivity() {
 
         // DnD
         val silence: Switch = findViewById(R.id.switchDnD)
+        silence.isChecked = getSharedPreferenceValueBool("silenceValue")
         var time = 5
 
         silence.setOnCheckedChangeListener{_, isChecked -> run {
+            writeToPreferenceBool("silenceValue", isChecked)
             if (isChecked) {
+                // If checked, a popup based on do_not_disturb.xml layout will appear.
                 val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
                 val view = inflater.inflate(R.layout.do_not_disturb, null)
                 val popupWindow = PopupWindow(
@@ -91,6 +98,8 @@ class AlertActivity : AppCompatActivity() {
                     LinearLayout.LayoutParams.WRAP_CONTENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT
                 )
+
+                // Fancy animations:
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
                     popupWindow.elevation = 10.0F
 
@@ -128,7 +137,8 @@ class AlertActivity : AppCompatActivity() {
                 }
 
                 popupWindow.setOnDismissListener {
-                    Toast.makeText(applicationContext, "Alerts disabled for ${data[time]}", Toast.LENGTH_SHORT).show()
+                    // TODO: bug: crashes when selecting first index.
+                    Toast.makeText(applicationContext, getString(R.string.disable_alerts, data[time]), Toast.LENGTH_SHORT).show()
                 }
 
                 popupWindow.showAtLocation(
@@ -138,21 +148,53 @@ class AlertActivity : AppCompatActivity() {
                 )
             } else {
                 time = 5
-                Toast.makeText(applicationContext, "Do not disturb cancelled.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, getString(R.string.cancel_disable_alerts), Toast.LENGTH_SHORT).show()
             }
         }}
 
         // DnD Week
-        val buttonWeek = findViewById<Button>(R.id.buttonWeek)
-        buttonWeek.setOnClickListener(object: View.OnClickListener {
-            override fun onClick(view: View): Unit {
+
+        // The textViews act as buttons for opening activity_week.xml layout.
+        val textViews: Array<TextView> = arrayOf(
+            findViewById(R.id.AlertWeekTitle),
+            findViewById(R.id.AlertWeekDesc)
+        )
+        for (i in textViews)
+            i.setOnClickListener {
                 val weekActivityIntent = Intent(this@AlertActivity, WeekActivity::class.java)
                 startActivityForResult(weekActivityIntent, 1)
                 recreate()
             }
-        })
+    }
+
+    // Gets the key from the static variable in sharedPref in MapsActivity
+    private fun getSharedPreferenceValueInt(prefKey: String):Int {
+        val sp = getSharedPreferences(MapsActivity.sharedPref, 0)
+        return sp.getInt(prefKey, 0)
 
     }
+
+    // When user changes a value onclick, this method is called for
+    private fun writeToPreferenceInt(prefKey:String, prefValue:Int) {
+        val editor = getSharedPreferences(MapsActivity.sharedPref, 0).edit()
+        editor.putInt(prefKey, prefValue)
+        editor.apply()
+    }
+
+    // Gets the key from the static variable in sharedPref in MapsActivity
+    private fun getSharedPreferenceValueBool(prefKey: String):Boolean {
+        val sp = getSharedPreferences(MapsActivity.sharedPref, 0)
+        return sp.getBoolean(prefKey, false)
+
+    }
+
+    // When user changes a value onclick, this method is called for
+    private fun writeToPreferenceBool(prefKey:String, prefValue:Boolean) {
+        val editor = getSharedPreferences(MapsActivity.sharedPref, 0).edit()
+        editor.putBoolean(prefKey, prefValue)
+        editor.apply()
+    }
+
     // Forces main activity to always recreate()
     override fun onBackPressed() {
         setResult(Activity.RESULT_OK, returnIntent)
--- app/src/main/java/com/example/pollution/ui/WeekActivity.kt
+++ app/src/main/java/com/example/pollution/ui/WeekActivity.kt
@@ -16,6 +16,7 @@ class WeekActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_week)
 
+        // Used for localisation.
         val weekdaysPlural = arrayOf(
             getString(R.string.week_monday_pl),
             getString(R.string.week_tuesday_pl),
@@ -25,9 +26,13 @@ class WeekActivity : AppCompatActivity() {
             getString(R.string.week_saturday_pl),
             getString(R.string.week_sunday_pl)
         )
-        val maxValues: Array<Double?> = Array(7) {i -> (0.00)}
-        val minValues: Array<Double?> = Array(7) {i -> (24.00)}
-        val seekbars: Array<RangeSeekBar<Double>> = arrayOf(
+
+        // Also used for localisation.
+        val maxValues: Array<Int> = Array(7) {i -> (0)}
+        val minValues: Array<Int> = Array(7) {i -> (24)}
+
+        // The seekBars and Switches: one for every day of the week.
+        val seekbars: Array<RangeSeekBar<Int>> = arrayOf(
             findViewById(R.id.seekBarMonday),
             findViewById(R.id.seekBarTuesday),
             findViewById(R.id.seekBarWednesday),
@@ -46,10 +51,16 @@ class WeekActivity : AppCompatActivity() {
             findViewById(R.id.switchSunday)
         )
 
+        // Every view will have the same functionality, so a loop will do the job.
         for (i in 0..6) {
-            seekbars[i].setRangeValues(0.00, 24.00)
-            seekbars[i].setOnRangeSeekBarChangeListener(object : RangeSeekBar.OnRangeSeekBarChangeListener<Double> {
-                override fun onRangeSeekBarValuesChanged(bar: RangeSeekBar<*>?, minValue: Double?, maxValue: Double?) {
+            // Save their states from last time.
+            switches[i].isChecked = getSharedPreferenceValueBool("switchValue$i")
+            // TODO: bug: the range values are reset on reopening the activity.
+            seekbars[i].setRangeValues(getSharedPreferenceValueInt("seekBarValue1$i"), getSharedPreferenceValueInt("seekBarValue2$i"))
+            seekbars[i].setOnRangeSeekBarChangeListener(object : RangeSeekBar.OnRangeSeekBarChangeListener<Int> {
+                override fun onRangeSeekBarValuesChanged(bar: RangeSeekBar<*>, minValue: Int, maxValue: Int) {
+                    writeToPreferenceInt("seekBarValue1$i", minValue)
+                    writeToPreferenceInt("seekBarValue2$i", maxValue)
                     minValues[i] = minValue
                     maxValues[i] = maxValue
                     Toast.makeText(applicationContext, getString(R.string.alert_time, minValues[i], maxValues[i]), Toast.LENGTH_SHORT).show()
@@ -57,12 +68,39 @@ class WeekActivity : AppCompatActivity() {
             })
 
             switches[i].setOnCheckedChangeListener{_, isChecked -> run {
+                writeToPreferenceBool("switchValue$i", isChecked)
                 if (isChecked)
                     Toast.makeText(applicationContext, getString(R.string.no_alert_weekday, minValues[i], maxValues[i], weekdaysPlural[i]), Toast.LENGTH_LONG).show()
                 else
                     Toast.makeText(applicationContext, getString(R.string.allow_alert_weekday, weekdaysPlural[i]), Toast.LENGTH_LONG).show()
             }}
         }
+    }
+
+    // Gets the key from the static variable in sharedPref in MapsActivity
+    private fun getSharedPreferenceValueInt(prefKey: String):Int {
+        val sp = getSharedPreferences(MapsActivity.sharedPref, 0)
+        return sp.getInt(prefKey, 0)
+    }
+
+    // When user changes a value onclick, this method is called for
+    private fun writeToPreferenceInt(prefKey:String, prefValue:Int) {
+        val editor = getSharedPreferences(MapsActivity.sharedPref, 0).edit()
+        editor.putInt(prefKey, prefValue)
+        editor.apply()
+    }
+
+    // Gets the key from the static variable in sharedPref in MapsActivity
+    private fun getSharedPreferenceValueBool(prefKey: String):Boolean {
+        val sp = getSharedPreferences(MapsActivity.sharedPref, 0)
+        return sp.getBoolean(prefKey, false)
+    }
+
+    // When user changes a value onclick, this method is called for
+    private fun writeToPreferenceBool(prefKey:String, prefValue:Boolean) {
+        val editor = getSharedPreferences(MapsActivity.sharedPref, 0).edit()
+        editor.putBoolean(prefKey, prefValue)
+        editor.apply()
     }
 
     // Forces main activity to always recreate()
